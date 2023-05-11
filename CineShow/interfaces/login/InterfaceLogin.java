f01647c96e2440ceec54b137d6a47fc81d5deb1e
emmanueln4ssif <emmanuel.nassif@gmail.com>
2023-05-10 21:31:54 -0300

    Adicionando Interface de Login ainda não funcional

---
+++ CineShow/interfaces/login/InterfaceLogin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfacelogin;
+
+/**
+ *
+ * @author emman
+ */
+public class InterfaceLogin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
