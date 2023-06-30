639f637a30ad2ab9c5d17e7124b6824f74242913
emmanueln4ssif <emmanuel.nassif@gmail.com>
2023-06-30 10:57:25 -0300

    Criando Excecoes para Filme e Ingresso

---
+++ CineShow/src/main/java/com/mycompany/cineshow/exceptions/FilmeException.java
@@ -0,0 +1,9 @@
+package com.mycompany.cineshow.exceptions;
+
+public class FilmeException extends Exception{
+    
+    public FilmeException(){
+        super("Nao foi possivel adicionar as alteracoes no Filme");
+    }
+
+}
---
+++ CineShow/src/main/java/com/mycompany/cineshow/exceptions/IngressoException.java
@@ -0,0 +1,9 @@
+package com.mycompany.cineshow.exceptions;
+
+public class IngressoException extends Exception{
+    
+    public IngressoException(){
+        super("Nao foi possivel efetuar a venda do ingresso!");
+    }
+
+}
